e9df8679a0429405e27180623e8a690ece5d1d21
yaowenjie <1743085737@qq.com>
2025-02-09 20:31:09 +0800

    通用分页对象

---
+++ src/main/java/com/mortal/wms/util/PageRequest.java
@@ -0,0 +1,20 @@
+package com.mortal.wms.util;
+
+import com.baomidou.mybatisplus.annotation.TableField;
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Schema(name = "分页查询")
+@Data
+public class PageRequest implements Serializable {
+    @TableField(exist = false)
+//    @ExcelIgnore
+    @Schema(name = "页码")
+    private Integer pageNum;
+    @TableField(exist = false)
+//    @ExcelIgnore
+    @Schema(name = "页长")
+    private Integer pageSize;
+}
